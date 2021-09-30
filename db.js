fb4c218201f9049fef11b54a96e39bbe560e6038
Hanna <Bethany.Hanna@autofinance.com>
2021-09-30 19:35:04 -0400

    updated in preperation for deployment to heroku

--- db.js
+++ db.js
@@ -1,11 +1,8 @@
 require("dotenv").config();
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize(
-    process.env.DB_DBNAME,
-    process.env.DB_USER,
-    process.env.DB_PASS,
+    process.env.DATABASE_URL,
     {
-        host: process.env.DB_HOST,
         dialect: 'postgres'
     }
 )
